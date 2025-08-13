2c75eff352adf518bf76a664d3d52f39e2ced81b
Keith131605 <kjamesglorioso@gmail.com>
2025-08-13 11:20:03 +0800

    Add files via upload

---
+++ code_challange1.py
@@ -0,0 +1,4 @@
+name = input("what is your name .?")
+
+
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\t\tHi\t\t\t\t*\n\t\t*\t\t\t", name, "\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
