f8fb7b6e7f49fb001a26a621b5864469b236bbd6
Keith131605 <kjamesglorioso@gmail.com>
2025-08-20 18:33:03 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,8 @@
+balance 500
+withdrawal = 1500
+b = 5
+a = 5
+namel = 'Glorioso'
+name2 = 'glorioso'
+#print(b >= a)
+print(name1 == name2)
---
+++ activity9.py
@@ -0,0 +1,7 @@
+username = "Glorioso'
+pasword = "kopality1234'
+
+#print (username == 'Glorioso')
+#print((username == 'Glorioso') and (password == 'kopality123'))
+#print((username == *Glorioso') or (password == 'kopality123'))
+#print(not((username == 'Glorioso") or (password == 'kopality123')))
