4ca7c12fc228a1958b5fbae3600112629c6f730d
Keith131605 <kjamesglorioso@gmail.com>
2025-09-24 10:17:00 +0800

    Add files via upload

---
+++ code_challenges10.py
@@ -0,0 +1,11 @@
+print("\t\t *",end='')
+for hotdog in range(1,11,1):
+    for hamburgir in range(10, hotdog, -1):
+        print("", end='  ')
+    for itlog in range(1, hotdog, 1):
+        print("*", end = ' ')
+    for EGG in range(1, hotdog, 1):
+        print("*", end=' ')
+    print()
+
+
