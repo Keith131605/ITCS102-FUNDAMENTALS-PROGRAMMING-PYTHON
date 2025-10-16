03044e4dcd49d7c01739bd62e49037fd7db6cd43
Keith131605 <kjamesglorioso@gmail.com>
2025-10-16 12:24:29 +0800

    Add files via upload

---
+++ activity21.py
@@ -0,0 +1,14 @@
+madungis = True
+
+while madungis == True:
+    confirm = input("Do you wish to continue washing(yes / no) -> ").lower()
+
+    if confirm == 'yes':
+        print("resume the cycle")
+        continue
+    elif confirm == 'no':
+        print("the cycle ends")
+        break
+    else:
+        print("invalid")
+        continue
