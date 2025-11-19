23cb5770bbde01ce43052d0511dd17fa6df1a94a
Keith131605 <kjamesglorioso@gmail.com>
2025-11-19 11:33:28 +0800

    Add files via upload

--- code_challenge16.py
+++ code_challenge16.py
@@ -6,14 +6,15 @@ print("-----------------------------------\n")
 student_records = {}
 
 while True:
-    print("SELECT FROM THE OPTION BELOW \nA - Add information\nB - Search a Recvord\nC - Delete a Record\nD - modify a Record\nE - Exit Program")
+    print("SELECT FROM THE OPTION BELOW \nA - Add information\nB - Record\nC - Search a Record\nD - Delete a Record\nE - Exit Program")
 
     choice = input("your choice --> ").lower()
 
     if choice == 'a':
+        os.system('cls')
         print("ADDING STUDENT INFORMATION")
         print("-------------------------------")
-        search_code = input("Key search for this student use this pattern(course-IDNO) --> ")
+        student_id = input("Key search for this student use this pattern(course-IDNO) --> ")
 
         first_name = input("Enter Student First Name --> ").upper()
         Last_name = input("Enter Student Last Name --> ").upper()
@@ -21,27 +22,63 @@ while True:
         email = input("Enter student email address --> ")
         isSingle = input("Are you single (True or False) --> ")
 
-        student_records = {search_code : [first_name, Last_name, course, email, isSingle ]}
+        student_records = {student_id : [first_name, Last_name, course, email, isSingle ]}
         print("DATA SAVED")
 
         os.system('cls')
         continue
 
     elif choice == 'b':
-        os.system('cls')
-        code = input("input search code --> ")
+        print("PRINTING STUDENT RECORD")
 
-        for j in student_records.keys():
-            if code in student_records.keys():
-                print("Record Found")
+        for id, record in student_records.items():
+            print(f"STUDENT ID {id} in STUDENT RECORD {record}")
 
-        os.system('cls')
         continue
+
     elif choice == 'c':
-        pass
+        os.system('cls')
+        print("SEARCH STUDENT RECORD")
+        print("============================")
+
+        search_id = input("Enter id to search -> ").lower()
+
+        for id in student_records.keys():
+            if search_id in student_records.keys():
+                print("===============================")
+                print("\n\nRECORD FOUND")
+                print("===============================")
+                # print the record of search student
+                for i in student_records[search_id]:
+                    print(f"-- {i}")
+            else:
+                print("===============================")
+                print("\n\nNO RECORD FOUND")
+                print("===============================")
+
         continue
     elif choice == 'd':
-        print("EDITING EXISTING RECORD")
+        os.system('cls')
+        print("DELETING EXISTING RECORD")
+
+        search_id = input("Enter id to search -> ").lower()
+
+        for id in student_records.keys():
+            if search_id in student_records.keys():
+                print("===============================")
+                print("\n\nRECORD FOUND")
+                print("===============================")
+                # print the record of search student
+                for i in student_records[search_id]:
+                    print(f"-- {i}")
+
+            student_records.pop(search_id)
+            print("RECORD DELETED")
+        else:
+            print("===============================")
+            print("\n\nNO RECORD FOUND")
+            print("===============================")
+
         continue
     elif choice == 'e':
         print("System Exit")
